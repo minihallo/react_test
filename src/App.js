df63756dd9be85467015ad07f5d3826c28b15de4
mini <loitermin@gmail.com>
2020-10-29 09:41:09 +0900

    1028 night

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Counter from './Counter';
 import Say from './Say';
 import React from 'react';
 import EventPractice from './EventPractice';
+import Validation from './ValidationSample';
+import RefSample from './RefSample';
 
 const App = () => {
   return (
     <div>
+      <RefSample /><br></br>
+      <Validation/>
       <EventPractice />
       {/* <Say />
       <Counter />
